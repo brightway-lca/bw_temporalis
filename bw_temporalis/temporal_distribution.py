bcb5968362082c66300392946fe999a499d86d79
Chris Mutel <cmutel@gmail.com>
2023-04-26 15:21:38 +0200

    Add temporal distribution

---
+++ bw_temporalis/temporal_distribution.py
@@ -0,0 +1,152 @@
+from bw2speedups import consolidate
+import numpy as np
+from numbers import Number
+
+
+class TemporalDistribution:
+    """A container for a series of values spread over time.
+    Args:
+        * *times* (ndarray): 1D array containg temporal info of `values` with type `timedelta64` or `datetime64` .
+        * *values* (ndarray): 1D array containg values with type `float`
+
+        Times and values must have same length and element of `values` must correspond to the element of `times`
+        with the same index.
+    """
+
+    def __init__(self, times, values):
+        if not isinstance(times, np.ndarray) or not isinstance(values, np.ndarray):
+            raise ValueError("Invalid input types")
+        if not times.shape == values.shape:
+            raise ValueError("Shapes of input `times` and `values` not identical")
+        if not (
+            np.issubdtype(times.dtype, np.datetime64)
+            or np.issubdtype(times.dtype, np.timedelta64)
+        ):
+            raise ValueError("Incorrect `times` dtype")
+
+        # Conversion needed for bw2speedups.consolidate function
+        self.values = values.astype(np.float64)
+
+        if np.issubdtype(times.dtype, np.datetime64):
+            self.times = times.astype("datetime64[D]")
+            self.base_time_type = "datetime64[D]"
+        elif np.issubdtype(times.dtype, np.timedelta64):
+            self.times = times.astype("timedelta64[D]")
+            self.base_time_type = "timedelta64[D]"
+        else:
+            raise ValueError("`times` must be numpy datetime or timedelta array")
+
+    def __len__(self):
+        return self.values.shape[0]
+
+    @property
+    def total(self):
+        return float(self.values.sum())
+
+    def nonzero(self):
+        mask = self.values == 0
+        return TemporalDistribution(self.times[mask], self.values[mask])
+
+    def __getitem__(self, index):
+        return TemporalDistribution(
+            np.array(self.times[index]), np.array(self.values[index])
+        )
+
+    def __mul__(self, other):
+        if isinstance(other, TemporalDistribution):
+            if (
+                self.base_time_type == "datetime64[D]"
+                and other.base_time_type == "datetime64[D]"
+            ):
+                raise ValueError("Can't multiple two datetime arrays")
+
+            times = (self.times.reshape((-1, 1)) + other.times.reshape((1, -1))).ravel()
+            values = (
+                self.values.reshape((-1, 1)) * other.values.reshape((1, -1))
+            ).ravel()
+
+            # Use array view in consolidate
+            # http://stackoverflow.com/a/33528073/4929813
+            t, v = consolidate(times.view("int64"), values)
+            return TemporalDistribution(t.astype(self.base_time_type), v)
+        else:
+            if self.base_time_type == "datetime64[D]":
+                raise ValueError("Can't multiply a datetime array")
+            try:
+                return TemporalDistribution(self.times, self.values * float(other))
+            except:
+                raise ValueError(
+                    "Can't multiply `TemporalDistribution` and {}".format(type(other))
+                )
+
+    def __truediv__(self, other):
+        if self.base_time_type == "datetime64[D]":
+            raise ValueError("Can't divide a datetime array")
+        elif not isinstance(other, Number):
+            raise ValueError("Can only divide time deltas by a number")
+        return TemporalDistribution(self.times, self.values / float(other))
+
+    def __lt__(self, other):
+        if not isinstance(other, TemporalDistribution):
+            return False
+        return self.values.sum() < other.values.sum()
+
+    def __add__(self, other):
+        if isinstance(other, TemporalDistribution):
+            if self.base_time_type == other.base_time_type == "datetime64[D]":
+                raise ValueError("Can't add two datetimes")
+            elif self.base_time_type == other.base_time_type == "timedelta64[D]":
+                times = np.hstack((self.times, other.times))
+                values = np.hstack((self.values, other.values))
+                # same as in __mul__
+                t, v = consolidate(times.view("int64"), values)
+                return TemporalDistribution(t.astype("timedelta64[D]"), v)
+            else:
+                if not len(self) == len(other):
+                    raise ValueError("Incompatible dimensions")
+                elif self.base_time_type == "timedelta64[D]":
+                    return TemporalDistribution(other.times + self.times, self.values + other.values)
+                else:
+                    return TemporalDistribution(self.times + other.times, self.values + other.values)
+        elif isinstance(other, Number):
+            return TemporalDistribution(self.times, self.values + float(other))
+        else:
+            raise ValueError(
+                "Can't add TemporalDistribution and {}".format(type(other))
+            )
+
+    def __iter__(self):
+        for index in range(len(self)):
+            yield (self.times[index], float(self.values[index]))
+
+    def __str__(self):
+        return "TemporalDistribution instance with %s values and total: %.4g" % (
+            len(self.values),
+            self.total,
+        )
+
+    def __repr__(self):
+        return (
+            "TemporalDistribution instance with %s values (total: %.4g, min: %.4g, max: %.4g"
+            % (len(self.values), self.total, self.values.min(), self.values.max())
+        )
+
+    def cumulative(self):
+        """Return new temporal distribution with cumulative values"""
+        return TemporalDistribution(self.times, np.cumsum(self.values))
+
+    # def datetime_to_timedelta(self, dt):
+    #     """Convert TD.times of type datetime64 to timedelta64 based on the datetime64 passed
+    #     """
+    #     assert 'datetime64' in str(self.times.dtype),'TemporalDistribution.times must be numpy.datetime64'
+    #     assert isinstance(dt,np.datetime64),'datetime must be numpy.datetime64'
+    #     return  TemporalDistribution(self.times - dt, self.values)
+
+    # def timedelta_to_datetime(self, dt):
+    #     """Convert TD.times of type timedelta64 to datetime.datetime based on the datetime64 passed
+    #     """
+    #     #converted to datetime.datetime cause timeline._groupby_sum_by_flow is ~5 slower when using np.datetime64
+    #     #while the conversion here to datatime.datetime is almost the same performance wise
+    #     assert 'timedelta64' in str(self.times.dtype),'TemporalDistribution.times must be numpy.datetime64'
+    #     assert isinstance(dt,np.datetime64),'datetime must be numpy.datetime64'
+    #     return  TemporalDistribution((self.times + dt).astype(datetime.datetime) , self.values)
---
+++ tests/td.py
@@ -0,0 +1,161 @@
+from bw_temporalis.temporal_distribution import TemporalDistribution as TD
+import numpy as np
+import pytest
+
+
+@pytest.fixture
+def simple():
+    return TD(np.arange(0, 5, dtype="timedelta64[D]"), np.ones(5) * 2)
+
+
+def test_init():
+    with pytest.raises(ValueError):
+        TD(None, None)
+    with pytest.raises(ValueError):
+        TD(np.arange(5), np.array([2, 2, 2, 2]))
+    with pytest.raises(ValueError):
+        TD(np.arange(5), np.ones(5) * 2)
+
+
+def test_mul_wrong_type(simple):
+    first = TD(np.arange(0, 5, dtype="datetime64[D]"), np.ones(5) * 2)
+    second = TD(np.array((-1, 0, 1), dtype="datetime64[D]"), np.ones(3).astype(float))
+    with pytest.raises(ValueError):
+        first * second
+    with pytest.raises(ValueError):
+        second * first
+
+
+def test_mul_wrong_type_two(simple):
+    first = TD(np.arange(0, 5, dtype="datetime64[D]"), np.ones(5) * 2)
+    with pytest.raises(ValueError):
+        first * 4.0
+
+
+def test_mul_td(simple):
+    td2 = TD(np.array((-1, 0, 1), dtype="timedelta64[D]"), np.ones(3).astype(float))
+
+    multiplied = simple * td2
+
+    print(multiplied.times)
+    print(multiplied.values)
+
+    assert np.array_equal(
+        np.arange(-1, 6, dtype="timedelta64[D]"),
+        multiplied.times,
+    )
+
+    assert simple.values.sum() * td2.values.sum() == multiplied.values.sum()
+
+    assert np.allclose(np.array((2.0, 4.0, 6.0, 6.0, 6.0, 4.0, 2.0)), multiplied.values)
+
+
+def test_div_td_error(simple):
+    td2 = TD(np.array((-1, 0, 1), dtype="datetime64[D]"), np.ones(3).astype(float))
+    with pytest.raises(ValueError):
+        td2 / 2
+
+
+def test_div_td_error_two(simple):
+    td2 = TD(np.array((-1, 0, 1), dtype="timedelta64[D]"), np.ones(3).astype(float))
+    with pytest.raises(ValueError):
+        td2 / "w00t"
+
+
+def test_div_int(simple):
+    """check possible division between td and int"""
+    divided = simple / 2.0
+    assert np.allclose(divided.values, 1)
+    assert np.array_equal(
+        np.arange(0, 5, dtype="timedelta64[D]"),
+        divided.times,
+    )
+
+
+def test_mul_int(simple):
+    simple *= 5
+    assert np.array_equal(
+        simple.times,
+        np.arange(0, 5, dtype="timedelta64[D]"),
+    )
+    assert np.allclose(simple.values, np.ones(5) * 10)
+
+
+def test_add_error_two_datetime(simple):
+    a = TD(np.arange(0, 5, dtype="datetime64[D]"), np.ones(5) * 2)
+    b = TD(np.arange(0, 5, dtype="datetime64[D]"), np.ones(5) * 2)
+    with pytest.raises(ValueError):
+        a + b
+
+
+def test_add_error_wrong_type(simple):
+    a = TD(np.arange(0, 5, dtype="datetime64[D]"), np.ones(5) * 2)
+    with pytest.raises(ValueError):
+        a + "w00t"
+
+
+def test_add_timedelta_error(simple):
+    a = TD(np.arange(0, 3, dtype="datetime64[D]"), np.ones(3))
+    with pytest.raises(ValueError):
+        simple + a
+    with pytest.raises(ValueError):
+        a + simple
+
+
+def test_add_number(simple):
+    simple += 5
+    assert np.array_equal(
+        simple.times,
+        np.arange(0, 5, dtype="timedelta64[D]"),
+    )
+    assert np.allclose(simple.values, np.ones(5) * 7)
+    simple += 0.5
+    assert np.array_equal(
+        simple.times,
+        np.arange(0, 5, dtype="timedelta64[D]"),
+    )
+    assert np.allclose(simple.values, np.ones(5) * 7.5)
+
+
+def test_add_two_tds(simple):
+    td2 = TD(np.array((-1, 0, 1), dtype="timedelta64[D]"), np.ones(3).astype(float))
+    added = simple + td2
+    assert np.array_equal(
+        np.array([-1, 0, 1, 2, 3, 4], dtype="timedelta64[D]"),
+        added.times,
+    )
+    assert added.values.sum() == (10 + 3)
+    assert np.array_equal(added.values, [1, 3, 3, 2, 2, 2])
+
+
+def test_add_td_to_dt(simple):
+    td2 = TD(np.array((-1, 0, 1, 2, 3), dtype="datetime64[D]"), np.ones(5))
+    added = simple + td2
+    expected = np.array([-1, 1, 3, 5, 7], dtype="datetime64[D]")
+    assert np.array_equal(expected, added.times)
+    assert np.array_equal(added.values, np.ones(5) * 3)
+
+
+def test_add_td_to_dt_error(simple):
+    td2 = TD(np.array((-1, 0, 1), dtype="datetime64[D]"), np.ones(3).astype(float))
+    with pytest.raises(ValueError):
+        simple + td2
+
+
+def test_iter(simple):
+    td = iter(simple)
+    assert next(td) == (np.timedelta64(0, "D"), 2)
+    assert next(td) == (np.timedelta64(1, "D"), 2)
+    assert next(td) == (np.timedelta64(2, "D"), 2)
+    assert next(td) == (np.timedelta64(3, "D"), 2)
+    assert next(td) == (np.timedelta64(4, "D"), 2)
+    with pytest.raises(StopIteration):
+        next(td)
+
+
+def test_str(simple):
+    assert str(simple)
+
+
+def test_repr(simple):
+    assert repr(simple)
