caa8d2a36684b1768e61b0c14bd58125c2e1a0b9
Chris Mutel <cmutel@gmail.com>
2023-04-27 13:42:06 +0200

    Add supplement_dataframe

--- bw_temporalis/__init__.py
+++ bw_temporalis/__init__.py
@@ -1,8 +1,8 @@
-__all__ = ("__version__", "Timeline", "TemporalDistribution", "TemporalisLCA")
+__all__ = ("__version__", "Timeline", "TemporalDistribution", "TemporalisLCA", "supplement_dataframe")
 
 from .lca import TemporalisLCA
 from .temporal_distribution import TemporalDistribution
 from .timeline import Timeline
-from .utils import get_version_tuple
+from .utils import get_version_tuple, supplement_dataframe
 
 __version__ = get_version_tuple()
--- bw_temporalis/utils.py
+++ bw_temporalis/utils.py
@@ -1,6 +1,9 @@
 import importlib.metadata
 from typing import Union
 
+import bw2data as bd
+import pandas as pd
+
 
 def get_version_tuple() -> tuple:
     def as_integer(x: str) -> Union[int, str]:
@@ -13,3 +16,25 @@ def get_version_tuple() -> tuple:
         as_integer(v)
         for v in importlib.metadata.version("bw_temporalis").strip().split(".")
     )
+
+
+def supplement_dataframe(df: pd.DataFrame, database_label: str, columns: list[str] | None = ["name", "location", "unit", "categories"]):
+    database_df = bd.Database(database_label).nodes_to_dataframe()
+    database_df['activity'] = database_df['flow'] = database_df['id']
+    df = df.join(
+        database_df.copy().drop(
+            columns=[c for c in database_df.columns if c not in (["flow"] + columns)]
+        ).set_index('flow'),
+        on='flow',
+        validate='m:1',
+        rsuffix="_flow"
+    )
+    df = df.join(
+        database_df.copy().drop(
+            columns=[c for c in database_df.columns if c not in (["activity"] + columns)]
+        ).set_index('activity'),
+        on='activity',
+        validate='m:1',
+        rsuffix="_activity"
+    )
+    return df.reset_index(drop=True)
