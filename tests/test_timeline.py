add13baff8f04d3f655d9fc4d793f36cdd4236e5
Michael <23102087+michaelweinold@users.noreply.github.com>
2023-04-27 10:23:52 +0200

    added markdown of expected dataframes

--- tests/test_timeline.py
+++ tests/test_timeline.py
@@ -3,32 +3,63 @@ import pandas as pd
 import numpy as np
 from bw_temporalis.timeline import Timeline
 
-df_input = pd.DataFrame(
-    data = {
-        'date': pd.Series(data=['20-12-2020', '21-12-2020', '15-12-2020', '16-12-2020', '25-05-2022', '26-05-2022'], dtype="datetime64[s]"),
-        'amount': pd.Series(data=[10.0, 9.0, 20.0, 19.0, 50.0, 49.0], dtype="float64"),
-        'flow': pd.Series(data=[1, 1, 1, 1, 3, 3], dtype="int"),
-        'activity': pd.Series(data=[2, 2, 2, 2, 4, 4], dtype="int")
-    }
-)
+def define_dataframes() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """
+    df_input:
 
-df_expected_characterize = pd.DataFrame(
-    data = {
-        'date': pd.Series(data=['20-12-2020', '21-12-2020', '15-12-2020', '16-12-2020', '25-05-2022', '26-05-2022'], dtype="datetime64[s]"),
-        'amount': pd.Series(data=[10.0, 9.0, 20.0, 19.0, 50.0, 49.0], dtype="float64"),
-        'flow': pd.Series(data=[1, 1, 1, 1, 3, 3], dtype="int"),
-        'activity': pd.Series(data=[2, 2, 2, 2, 4, 4], dtype="int")
-    }
-)
+    | date       | amount | flow | activity |
+    |------------|--------|------|----------|
+    | 20-12-2020 | 10     | 1    | 2        |
+    | 15-12-2020 | 20     | 1    | 2        |
+    | 25-05-2022 | 50     | 3    | 4        |
 
-df_expected_sum_days_to_years = pd.DataFrame(
-    data = {
-        'date': pd.Series(data=['01-01-2020', '01-01-2022'], dtype="datetime64[s]"),
-        'amount': pd.Series(data=[39.0, 99.0], dtype="float64"),
-        'flow': pd.Series(data=[1, 1], dtype="int"),
-        'activity': pd.Series(data=[2, 4], dtype="int")
-    }
-)
+    df_expected_characterize:
+
+    | date       | amount | flow | activity |
+    |------------|--------|------|----------|
+    | 20-12-2020 | 10     | 1    | 2        |
+    | 21-12-2020 | 9      | 1    | 2        |
+    | 15-12-2020 | 20     | 1    | 2        |
+    | 16-12-2020 | 19     | 1    | 2        |
+    | 25-05-2022 | 50     | 3    | 4        |
+    | 26-05-2022 | 49     | 3    | 4        |
+
+    df_expected_sum_days_to_years:
+
+    | date | amount | flow | activity |
+    |------|--------|------|----------|
+    | 2020 | 39     | 1    | 2        |
+    | 2022 | 99     | 1    | 4        |
+    """
+
+    df_input = pd.DataFrame(
+        data = {
+            'date': pd.Series(data=['20-12-2020', '21-12-2020', '15-12-2020', '16-12-2020', '25-05-2022', '26-05-2022'], dtype="datetime64[s]"),
+            'amount': pd.Series(data=[10.0, 9.0, 20.0, 19.0, 50.0, 49.0], dtype="float64"),
+            'flow': pd.Series(data=[1, 1, 1, 1, 3, 3], dtype="int"),
+            'activity': pd.Series(data=[2, 2, 2, 2, 4, 4], dtype="int")
+        }
+    )
+
+    df_expected_characterize = pd.DataFrame(
+        data = {
+            'date': pd.Series(data=['20-12-2020', '21-12-2020', '15-12-2020', '16-12-2020', '25-05-2022', '26-05-2022'], dtype="datetime64[s]"),
+            'amount': pd.Series(data=[10.0, 9.0, 20.0, 19.0, 50.0, 49.0], dtype="float64"),
+            'flow': pd.Series(data=[1, 1, 1, 1, 3, 3], dtype="int"),
+            'activity': pd.Series(data=[2, 2, 2, 2, 4, 4], dtype="int")
+        }
+    )
+
+    df_expected_sum_days_to_years = pd.DataFrame(
+        data = {
+            'date': pd.Series(data=['01-01-2020', '01-01-2022'], dtype="datetime64[s]"),
+            'amount': pd.Series(data=[39.0, 99.0], dtype="float64"),
+            'flow': pd.Series(data=[1, 1], dtype="int"),
+            'activity': pd.Series(data=[2, 4], dtype="int")
+        }
+    )
+
+    return(df_input, df_expected_characterize, df_expected_sum_days_to_years)
 
 
 def function_characterization_test(series: pd.Series, period: int = 2) -> pd.DataFrame:
