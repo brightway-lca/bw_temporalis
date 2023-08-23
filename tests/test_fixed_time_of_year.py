6b6141fef590d1649a5ebb2d3cddfc27e10d0370
Chris Mutel <cmutel@gmail.com>
2023-08-23 21:59:56 +0200

    Update test_fixed_time_of_year.py

--- tests/test_fixed_time_of_year.py
+++ tests/test_fixed_time_of_year.py
@@ -17,6 +17,12 @@ def test_ftoy_negative_error():
         FixedTimeOfYearTD(td.date, td.amount)
 
 
+def test_ftoy_past_one_year_error():
+    td = easy_timedelta_distribution(350, 370, resolution="D", steps=10)
+    with pytest.raises(ValueError):
+        FixedTimeOfYearTD(td.date, td.amount)
+
+
 def test_ftoy_multiplication_simple():
     td = easy_timedelta_distribution(0, 60, resolution="D", steps=4)
     ftoy = FixedTimeOfYearTD(td.date, td.amount)
@@ -38,6 +44,50 @@ def test_ftoy_multiplication_simple():
         ),
         amount=np.array(
             [1 / 16, 1 / 16, 1 / 16, 1 / 16, 3 / 16, 3 / 16, 3 / 16, 3 / 16]
+        ),
+    )
+    assert np.array_equal(result.date, expected.date)
+    assert np.array_equal(result.amount, expected.amount)
+
+
+def test_ftoy_multiplication_td_spans_one_year():
+    td = easy_timedelta_distribution(0, 60, resolution="D", steps=4)
+    ftoy = FixedTimeOfYearTD(td.date, td.amount)
+    atd = easy_datetime_distribution(start="2020-03-01", end="2021-09-01", steps=6)
+    result = atd * ftoy
+    expected = TemporalDistribution(
+        date=np.array(
+            [
+                "2019-01-01",
+                "2019-01-21",
+                "2019-02-10",
+                "2019-03-02",
+                "2020-01-01",
+                "2020-01-21",
+                "2020-02-10",
+                "2020-03-01",
+                "2021-01-01",
+                "2021-01-21",
+                "2021-02-10",
+                "2021-03-02",
+            ],
+            dtype="datetime64[s]",
+        ),
+        amount=np.array(
+            [
+                1 / 24,
+                1 / 24,
+                1 / 24,
+                1 / 24,
+                3 / 24,
+                3 / 24,
+                3 / 24,
+                3 / 24,
+                2 / 24,
+                2 / 24,
+                2 / 24,
+                2 / 24,
+            ]
         ),
     )
     assert np.array_equal(result.date, expected.date)
